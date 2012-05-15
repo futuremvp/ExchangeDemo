b4bdaed6e8f911381e5b3861fb477c36531dba3c
Cyril Wei <teakscn@gmail.com>
2012-05-15 15:47:44 +1000

    Cyril: add iOS project with Frank

---
+++ ios-core/Application/TWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWAppDelegate.h
+//  ExchangeDemo
+//
+//  Created by Cyril Wei on 5/15/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-core/Application/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExchangeDemo
+//
+//  Created by Cyril Wei on 5/15/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
